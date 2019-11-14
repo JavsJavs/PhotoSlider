442c28fc4835406411fb3e13b083ec84fd4db87c
JavsJavs <45043131+JavsJavs@users.noreply.github.com>
2019-11-14 18:48:27 +0100

    final finish

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-function prevPhoto() {
-    var currentPhoto = document.getElementById("slider").src;
-    console.log(currentPhoto);
-    switch (currentPhoto) {
-        case 'Photos/2ShrekWzowski.jpg':
-            document.getElementById('slider').src = 'Photos/1ThanosHomer.jpg';
-            break;
-        case 'Photos/3KeanuSmall.jpg':
-            document.getElementById('slider').src = 'Photos/2ShrekWzowski.jpg';
-            break;
-        case 'Photos/4Mesi.png':
-            document.getElementById('slider').src = 'Photos/3KeanuSmall.jpg';
-            break;
-        case 'Photos/5SullyMike.jpg':
-            document.getElementById('slider').src = 'Photos/4Mesi.png';
-            break;
-        default:
-            break;
+var currentPhoto = 1;
+
+function movePhoto(dir) {
+    if (dir === 0 && currentPhoto != 1)
+            currentPhoto -= 1;
+    else if (dir === 1 && currentPhoto != 5)
+            currentPhoto += 1;
+    document.getElementById('slider').src = 'Photos/photo' + currentPhoto + '.jpg';
+    check();
+}
+
+function check() {
+    if(currentPhoto === 1) {
+        changeButton(0, 0);
+    }else if(currentPhoto === 5){
+        changeButton(1, 0);
+    }else{
+        changeButton(0, 1);
+        changeButton(1, 1);
     }
 }
 
-function nextPhoto() {
-    var currentPhoto = document.getElementById("slider").src;
-    console.log(currentPhoto);
-    switch (currentPhoto) {
-        case 'Photos/1ThanosHomer.jpg':
-            document.getElementById('slider').src = 'Photos/2ShrekWzowski.jpg';
-            break;
-        case 'Photos/2ShrekWzowski.jpg':
-            document.getElementById('slider').src = 'Photos/3KeanuSmall.jpg';
-            break;
-        case 'Photos/3KeanuSmall.jpg':
-            document.getElementById('slider').src = 'Photos/4Mesi.png';
-            break;
-        case 'Photos/4Mesi.png':
-            document.getElementById('slider').src = 'Photos/5SullyMike.jpg';
-            break;
-        default:
-            break;
-
+function changeButton(whichButton, blockUnblock) {
+    if(blockUnblock === 0) {
+        if (whichButton === 0) {
+            document.getElementById('prevButton').style.color = "lightgray";
+            document.getElementById('prevButton').disabled = true;
+        } else if (whichButton === 1) {
+            document.getElementById('nextButton').style.color = "lightgray";
+            document.getElementById('nextButton').disabled = true;
+        }
+    }else if(blockUnblock === 1){
+        if (whichButton === 0) {
+            document.getElementById('prevButton').style.color = "black";
+            document.getElementById('prevButton').disabled = false;
+        } else if (whichButton === 1) {
+            document.getElementById('nextButton').style.color = "black";
+            document.getElementById('nextButton').disabled = false;
+        }
     }
 }
